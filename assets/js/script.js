17c7b2535a1ca92f29df763d2ded5610fe9af3c2
danukaj <damayanthiamarakoon753@gmail.com>
2023-10-11 15:14:41 +0530

    app process

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+import 'assets\js\anime.es.js';
+import 'assets\js\anime.js';
+import 'assets\js\anime.min.js';
+import 'assets\js\bootstrap.bundle.js';
+import 'assets\js\bootstrap.bundle.min.js';
+import 'assets\js\bootstrap.esm.js';
+import 'assets\js\bootstrap.esm.min.js';
+import 'assets\js\bootstrap.js';
+import 'assets\js\bootstrap.min.js';
+
