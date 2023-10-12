36f027b57d79483ade9353db5131412a05037c60
danukaj <damayanthiamarakoon753@gmail.com>
2023-10-12 18:24:29 +0530

    home animation

--- header.php
+++ header.php
@@ -4,21 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="style.css">
+    <style>
+        .random-color {
+            color: #<?php echo dechex(rand(0x000000, 0xEEEEEE)); ?>
+        }
+    </style>
 </head>
 
 <body class="">
     <nav class="navbar  fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="assets/resources/logo.png" class="images" alt="">
+                <img src="resources/logo.png" class="images" alt="">
             </a>
             <button class="navbar-toggler text-bg-info " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="offcanvas bg-dark text-light fw-bolder offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                    <h5 class="offcanvas-title random-color text-capitalize" id="offcanvasNavbarLabel">
                         <?php
                         if (isset($_SESSION['userData'])) {
                             $discord_id = $_SESSION['userData']['discord_id'];
@@ -42,19 +47,19 @@
                 <div class=" text-light offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                         <li class="nav-item">
-                            <a class="nav-link text-light active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link text-light active" aria-current="page" href="../index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Application</a>
+                            <a class="nav-link text-light" href="../application.php">Application</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Profile</a>
+                            <a class="nav-link text-light" href="../playerDashboard.php">Profile</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="#">Streamer</a>
+                            <a class="nav-link text-light" href="../streamers.php">Streamer</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link text-light" href="#">About Us</a>
+                            <a class="nav-link text-light" href="../aboutus.php">About Us</a>
                         </li>
                     </ul>
                 </div>
--- index.php
+++ index.php
@@ -5,10 +5,17 @@ require "database/connection.php";
 
 $_SESSION['userData'];
 $_SESSION['logged_in'];
-if(empty($_GET['msg'])){
-    $msg = "";  
-}else{
+if (empty($_GET['msg'])) {
+    $msg = "";
+} else {
     $mes = $_GET['msg'];
+?>
+    <script>
+        //scripts anime.js
+        alert("<?php echo $mes ?>")
+        window.location = "index.php";
+    </script>
+<?php
 }
 
 //$avatar_url = "https://cdn.discordapp.com/avatars/$discord_id/$avatar.jpg";
@@ -21,19 +28,20 @@ if(empty($_GET['msg'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COLOMBO CITY&copy; || ROLEPLAY&trade;</title>
-    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="..\style.css">
     <link rel="stylesheet" href="assets\css\font.css">
     <link rel="stylesheet" href="assets\css\bootstrap.css">
     <link rel="stylesheet" href="assets\css\bootstrap.min.css">
     <style>
         .background {
-            background-image: url('assets/resources/homebg.jpg');
+            background-image: url('resources/homebg.jpg');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
-            z-index: -12;
+            display: flex;
+            z-index: -99992;
             height: 100vh;
-            position: relative;
+            position: absolute;
             filter: blur(5px);
             -webkit-filter: blur(5px);
             /* Webkit (Safari) */
@@ -54,24 +62,84 @@ if(empty($_GET['msg'])){
                 <?php require "header.php" ?>
                 <span class="text-warning h1"></span>
             </div>
-            <div class="col-12 background">
+            <div class=" background">
             </div>
-            <div class="col-12 justify-content-center">
+            <div class="col-12 ">
+            <div class=" col-12   container111"></div>
+                <div style="height: 100vh;" class="row  align-items-center justify-content-center ">
+                    <div class="col-12  col-md-12 justify-content-center mt-5 col-lg-5">
+                        <div class="text-light mx-5 text-uppercase sl h3">
+                            <span>colombo city roleplay next level</span>
+                        </div>
+                        <div class="text-uppercase mx-5 rpl text-light">
+                            <span style="
+                            
+                            background: linear-gradient(5deg, blue,rgb(132, 132, 245));
+                            background-clip: text;
+                            -webkit-background-clip: text;
+                            color: transparent;
+                            display: inline-block;
+                            animation: snake 10s linear infinite; 
+                            " class="shadow">roleplay</span>
+                        </div>
+                        <div class="text-uppercase mx-5 fw-bold h2 cc text-light">
+                            <span style="
+                            background: linear-gradient(45deg, pink,red);
+                            background-clip: text;
+                            -webkit-background-clip: text;
+                            color: transparent;
+                            display: inline-block;
+                            animation: snake 10s linear infinite; 
+                            font-weight:bolder;
+                            ">colombo city roleplay </span><br>
+                        </div>
+                        <a href="#" class="mx-5"><span class="btn btn-info span1 text-light fw-bolder" style="border-radius: 25px;">Whitelist &RightArrow;</span></a>
+                    </div>
+                    <div class="col-lg-5  d-none d-lg-block ">
+                        <div class="row ">
+                            <div style="height: 100vh;" class="col-12 bgbg d-block ">
+                            
+                            </div>
+                            
+                        </div>
+                    </div>
 
+                </div>
             </div>
         </div>
     </div>
-    <script src="assets/js/script.js"></script>
-    <script src="assets/js/anime.js"></script>
-    <script src="assets/js/anime.es.js"></script>
     <script src="assets/js/anime.min.js"></script>
+    <script>
+        const container = document.querySelector(".container111"); // Use a dot (.) to select by class name
+        for (let i = 0; i < 200; i++) {
+            let blocks = document.createElement('div');
+            blocks.classList.add('block');
+            container.appendChild(blocks);
+        }
+        function animateBlocks(){
+            anime({
+                targets: '.block',
+                translateX:function(){
+                    return anime.random(-800, 800);
+                },
+                translateY:function(){
+                    return anime.random(-600, 600);
+                },
+                scale:function(){
+                    return anime.random(1, 5);
+                },
+                easing: 'linear',
+                duration:1000,
+                delay:anime.stagger(10),
+                complete: animateBlocks,
+            })
+        }
+        animateBlocks()
+    </script>
+    <script src="assets/js/script.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
 
-    <script>
-        //scripts anime.js
-        alert("<?php echo $mes ?>")
-        window.location= "index.php";
-    </script>
+
 </body>
 
 </html>
